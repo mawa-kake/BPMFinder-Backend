b051d73f65a7fe51e9994ade4b91e3c96797d4c9
Karan Marwah <karanmarwah@Karans-MacBook-Air.local>
2017-10-31 22:58:45 -0400

    Fixed server listener for dynamic port alotting

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import {getTracks} from './spotifyFetch'
 // Initialize http server
 const app = express()
 
+//dynamically listens for heroku port
+let port = process.env.PORT || 8080;
+
 // Handle / route
 app.get('/:query', (req, res) => {
     getTracks(req.params.query).then((data) => {res.send(data)})
@@ -16,7 +19,7 @@ app.get('/:query', (req, res) => {
 app.use(morgan('combined'))
 
 // Launch the server on port 3000
-const server = app.listen(3000, () => {
+const server = app.listen(port, () => {
     const { address, port } = server.address()
     console.log(`Listening at http://${address}:${port}`) //eslint-disable-line
 });
